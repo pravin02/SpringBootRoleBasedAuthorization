b90634a50b5de0bae3add854b38802123b93e1d6
Pravin P Patil <prvnpatil11@gmail.com>
2020-11-21 14:43:56 +0530

    starter template

---
+++ src/main/java/org/pk/rolebasedauth/RoleBasedAuthorization/RoleBasedAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package org.pk.rolebasedauth.RoleBasedAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pk/rolebasedauth/RoleBasedAuthorization/RoleBasedAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pk.rolebasedauth.RoleBasedAuthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
