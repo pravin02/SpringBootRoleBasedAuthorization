bc20db6955f57c1d0d746c3cb91abc35e8c0fc5e
Pravin P Patil <prvnpatil11@gmail.com>
2020-11-21 17:17:36 +0530

    role based implemented

--- src/main/java/org/pk/rolebasedauth/entities/User.java
+++ src/main/java/org/pk/rolebasedauth/entities/User.java
@@ -5,7 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 
 import java.io.Serializable;
 
@@ -20,12 +25,19 @@ public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private int userId;
+    @Column(name = "username")
     private String username;
+    @Column(name = "password")
     private String password;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "roles")
     private String roles;
+    @Column(name = "is_active")
     private boolean isActive;
 
 }
